d348dddfa3c74b49c6394bcb007101ba43deed3a
dajacobs <dajacobs.tab@gmail.com>
2015-11-06 19:34:03 -0600

    message/packet func

--- src/rtp.c
+++ src/rtp.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #define BIDIRECTIONAL 1
+
+// Character message data
+struct msg {
+	char data[20];
+};
+
+// Packet function
+struct pkt {
+	int seqnum;
+	int acknum;
+	int checksum;
+	char payload[20];
+};
