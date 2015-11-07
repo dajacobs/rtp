7a70705c0159455cef958c67b6f91327a0b6cc50
dajacobs <dajacobs.tab@gmail.com>
2015-11-06 19:40:05 -0600

    event struct

--- src/rtp.c
+++ src/rtp.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #define BIDIRECTIONAL 1
 
-// Character message data
+// Character message data structure
 struct msg {
 	char data[20];
 };
 
-// Packet function
+// Packet structure
 struct pkt {
 	int seqnum;
 	int acknum;
 	int checksum;
 	char payload[20];
+};
+
+/* Network emulation */
+
+// Event structure
+struct event {
+	// Event time
+	float evtime;
+	// Event type code
+	int evtype;
+	// Event entity of occurence
+	int eventity;
+	// Pointer to packet with event
+	struct pkt *pktptr;
+	// Previous event occured
+	struct event *prev;
+	// Next event occured
+	struct event *next;
 };
